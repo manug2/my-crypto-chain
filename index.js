6ed58acf92483bb0cd102d325e8997dd81ef3a58
manu <manugarg2@gmail.com>
2025-12-30 17:11:39 +0800

    sync chain with root node for non-root peers using get api request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const Block = require('./src/block');
 const Blockchain = require('./src/blockchain');
 const PubSub = require('./src/pubsub');
@@ -8,10 +9,11 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-
-setTimeout(() => pubsub.broadcastChain(), 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 app.use(bodyParser.json());
+
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
@@ -27,8 +29,18 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+    if (! error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body);
 
-const DEFAULT_PORT = 3000;
+      console.log('replace chain on a sync with root: \n', rootChain);
+      blockchain.replaceChain(rootChain);
+    }
+  });
+};
+
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
@@ -39,5 +51,8 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
   console.log(`listening at port localhost:${PORT}`);
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
 
