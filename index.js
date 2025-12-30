bf36c4be5c1e17591a7369091e04299881856591
manu <manugarg2@gmail.com>
2025-12-30 16:55:43 +0800

    add redis-server for pub sub capability. add dev peers for starting blockchain instances on random ports. broadcast mined blocks.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Block = require('./src/block');
 const Blockchain = require('./src/blockchain');
+const PubSub = require('./src/pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
 
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -17,10 +22,22 @@ app.post('/api/mine', (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
 app.listen(PORT, () => {
   console.log(`listening at port localhost:${PORT}`);
 });
+
